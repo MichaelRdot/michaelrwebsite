26773a20c61c05d291796b9cc5dec7d24552b9e8
MichaelRdot <mruss62@lsu.edu>
2026-02-24 13:45:13 -0600

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Header from './components/Header.jsx';
+
+function App() {
+    return (
+        <>
+           <Header/>
+            <div className="container-xl">
+
+            </div>
+        </>
+    )
+}
+
+export default App;
