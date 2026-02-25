cd72c8446788b24f65f2142e915d89d231f0e0c9
MichaelRdot <mruss62@lsu.edu>
2026-02-24 18:15:52 -0600

    Made some changes to the header

--- src/components/Header & Footer/Header.jsx
+++ src/components/Header & Footer/Header.jsx
@@ -3,9 +3,17 @@
 function Header() {
     return (
         <header className="header">
+            <div className="header-name">Michael Russelburg</div>
             <nav>
-
+                <a href="#aboutMeSection">About Me </a>
+                <a href="#projectsSection">Projects </a>
+                <a href="#educationSection">Education </a>
+                <a href="#workSection">Work </a>
+                <a href="#hobbiesSection">Hobbies </a>
             </nav>
+            <a href="https://github.com/MichaelRdot" target="_blank" rel="noreferrer" className="header-github">
+                GitHub
+            </a>
         </header>
     )
 }
