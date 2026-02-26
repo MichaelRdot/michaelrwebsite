ccc5d1649d26ed43ab8f25903cef61ba98a3efd7
MichaelRdot <mruss62@lsu.edu>
2026-02-25 23:58:35 -0600

    Added hobbies

--- src/components/Hobbies Section/HobbiesSection.jsx
+++ src/components/Hobbies Section/HobbiesSection.jsx
@@ -2,29 +2,33 @@
 
 const hobbies = [
     {
-        name: 'Magic: The Gathering',
-        traits: ['Social', 'Competitive', 'Creative'],
-        description: '.',
+        name: 'Bass Guitar',
+        traits: ['Creative', 'Solo'],
+        description: 'I really only just started this one. Do not ask me about it.',
     },
     {
         name: 'Board Games',
-        traits: ['Social', 'Strategic'],
-        description: '.',
-    },
-    {
-        name: 'Video Games',
-        traits: ['Solo', 'Competitive'],
-        description: '.',
-    },
-    {
-        name: 'Bass Guitar',
-        traits: ['Creative', 'Solo'],
-        description: '.',
+        traits: ['Social', 'Competitive', 'Strategic'],
+        description: 'My current favorite board game is this cute war game called Root. Each player commands a faction ' +
+            'forces players to each score victory points in a unique and interesting way.',
     },
     {
         name: 'Gym',
-        traits: ['Solo', 'Discipline'],
-        description: '.',
+        traits: ['Social', 'Discipline'],
+        description: 'I have been going to the gym less and less recently, but is that not true for all of us?'
+    },
+    {
+        name: 'Magic: The Gathering',
+        traits: ['Social', 'Competitive', 'Creative', 'Strategic'],
+        description: 'Magic: the Gathering is a card game that I picked up when I was 7. Since then it has been a thing ' +
+            'that I play on and off. Currently, I enjoy playing Commander with my friends either online or in person.',
+    },
+
+    {
+        name: 'Video Games',
+        traits: ['Social', 'Solo', 'Competitive', 'Creative', 'Strategic'],
+        description: 'This one is an obvious since I am a male in computer science, but my favorites include Terraria, ' +
+            'Slay the Princess, and Stardew Valley',
     },
 ]
 
