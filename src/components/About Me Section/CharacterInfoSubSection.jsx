a21ead71029767c4df327b062d2563267ee2b6b5
MichaelRdot <mruss62@lsu.edu>
2026-02-25 23:43:47 -0600

    Meows are in

--- src/components/About Me Section/CharacterInfoSubSection.jsx
+++ src/components/About Me Section/CharacterInfoSubSection.jsx
@@ -1,15 +1,23 @@
 ﻿import './CharacterInfoSubSection.css';
 import './AboutMeSection.css';
-import Button from "bootstrap/js/src/button.js";
 
 function CharacterInfoSubSection() {
+    const meows = ['/Cat Call.m4a', '/Chill Meow.m4a', '/Meow #1.m4a', '/Meow Deep.m4a', '/Meowth.m4a', '/Ruff.m4a', '/Sassy Meow.m4a', '/Sparky Meow.m4a']
+
+    const playRandomMeow = () => {
+        const random = meows[Math.floor(Math.random() * meows.length)]
+        const audio = new Audio(random)
+        audio.currentTime = 0
+        audio.play()
+    }
+
     return (
         <>
             <div className="about-title-bar">Character Info</div>
             <div className="about-row-1">
 
                 {/* Photo */}
-                <button className="photo-container">
+                <button className="photo-container" onClick={() => playRandomMeow()}>
                     <div className="photo-hover-wrapper">
                         <img src="/Miraak, My Boy!!.jpg" className="photo-normal" alt="Portrait" />
                         <img src="/Miraak, My (Evil) Boy!!.jpg" className="photo-hover" alt="Portrait" />
